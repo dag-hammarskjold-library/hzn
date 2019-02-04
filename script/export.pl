4ea97100592e7db6dd34117ab4860e0fb9d5ee46
Joseph Bukhari <Joseph.Bukhari@UNHQ.UN.ORG>
2019-02-04 15:49:21 -0500

    adapt to changes in lib

--- script/export.pl
+++ script/export.pl
@@ -7,6 +7,7 @@ use Getopt::Std;
 use Data::Dumper;
 $Data::Dumper::Indent = 1;
 use FindBin;
+use List::Util qw<none>;
 
 # dist
 
@@ -16,11 +17,11 @@ use Hzn::SQL::MARC;
 
 use Hzn::Export::Auth;
 use Hzn::Export::Auth::DLX;
-use Hzn::Export::Auth::DLX::Tind;
+use Hzn::Export::Auth::DLX::UNDL;
 
 use Hzn::Export::Bib;
 use Hzn::Export::Bib::DLX;
-use Hzn::Export::Bib::DLX::Tind;
+use Hzn::Export::Bib::DLX::UNDL;
 
 package main;
 
@@ -39,18 +40,27 @@ sub options {
 		['o:' => 'output file'],
 		['m:' => 'modified since'],
 		['u:' => 'modified_until'],
+		['D' => 'export in DLX mode'],
+		['U' => 'export in UNDL mode'],
+		['X' => 'export as XML'],
+		['C' => 'export as MARC21 (.mrc)'],
+		['K' => 'export as .mrk'],
+		['B' => 'write exported data to MongoDB (as BSON)'],
 		['M:' => 'MongoDB connection string'],
-		['t:' => 'export type ("DLX", "Tind"); defaults to raw MARC'],
-		['3:' => 's3 db'], # deprecated
 	);
+	
 	my @copy = @ARGV;
 	getopts (join('', map {$_->[0]} @opts), \my %opts);
 	if (! %opts || $opts{h}) {
 		say join ' - ', @$_ for @opts;
 		exit;
 	}
-	$opts{$_} || die "required opt $_ missing\n" for qw||;
-	-e $opts{$_} || die qq|"$opts{$_}" is an invalid path\n| for qw||;
+	
+	VALIDATE: {
+		(none {$opts{$_}} qw<X C K B>) && die "X, C, K, or B required";
+		$opts{B} || $opts{U} && ! $opts{M} && die "-M required with -B and -U";
+		
+	}
 	
 	$opts{ARGV} = \@copy;
 	return \%opts;
@@ -61,33 +71,26 @@ sub MAIN {
 		
 	my $class = do {
 		my $class = 'Hzn::Export::'.($opts->{a} ? 'Auth' : 'Bib');
-		$class .= '::DLX' if $opts->{t};
-		$class .= '::Tind' if $opts->{t} && $opts->{t} eq 'Tind';
+		$class .= '::DLX' if $opts->{D};
+		$class .= '::DLX::UNDL' if $opts->{U};
 		$class;
 	};
 	
 	my $export = $class->new; #
 	
 	$opts->{s} && $export->sql_criteria($opts->{s});
+	
 	$opts->{m} && $export->modified_since($opts->{m});
 	$opts->{u} && $export->modified_until($opts->{u});
 	
-	if ($opts->{f}) {
-		$export->output_type($opts->{f});
-		$export->output_filename($opts->{o}),
-	} elsif ($opts->{M}) {
-		require MongoDB;
-		$export->mongodb_collection_handle (
-			MongoDB->connect($opts->{M})
-			->get_database('undlFiles')
-			->get_collection(($opts->{a} ? 'auth' : 'bib').'_JMARC')
-		)
-	}
+	$opts->{M} && $export->mongodb_connection_string($opts->{M});
 	
-	if ($opts->{t} && $opts->{t} eq 'Tind') {
-		die 's3 db not found' unless -e $opts->{3};
-		$export->s3_db_path($opts->{3});
-	}
+	$opts->{X} && $export->output_type('xml');
+	$opts->{C} && $export->output_type('mrc');
+	$opts->{K} && $export->output_type('mrk');
+	$opts->{B} && $export->output_type('mongo');
 	
+	$opts->{o} && $export->output_filename($opts->{o});
+
 	$export->run;
 }
