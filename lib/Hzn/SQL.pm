596c61c1da1f60e078164cced28d25dc9192df88
jbukhari <joe.bukhari@gmail.com>
2020-05-29 13:50:11 -0400

    Hard code temp dir

--- lib/Hzn/SQL.pm
+++ lib/Hzn/SQL.pm
@@ -41,7 +41,8 @@ has 'temp_script' => (
 		my $self = shift;
 		
 		confess q{Attribute "statement" or "script" must be set} unless $self->statement;		
-		my $tempdir = $ENV{HOMEPATH}.'/temp';
+		#my $tempdir = $ENV{HOMEPATH}.'/temp';
+		my $tempdir = 'C:/temp';
 		mkdir $tempdir;
 		my $path = "$tempdir/".time.'_'.rand().'.sql';
 		open my $temp,'>',$path or die $!;
