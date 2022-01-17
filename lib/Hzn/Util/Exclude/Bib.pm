0d7440194d094bf39ec42d88f005c30eb31883f7
jbukhari <joe.bukhari@gmail.com>
2022-01-17 16:59:08 -0500

    Update Bib.pm

--- lib/Hzn/Util/Exclude/Bib.pm
+++ lib/Hzn/Util/Exclude/Bib.pm
@@ -9,14 +9,17 @@ has 'sql' => (
 	default => q {
 		select bib# 
 		from bib_control 
-		where bib# not in (
-			select bib# from bib where tag in ("191","791")
-		) 
-		and bib# not in (
-			select bib# from bib where tag = "099" and text like "%bDHU%"
-		)
-		or bib# in (
-			select bib# from bib where tag = "000" and substring(text,6,1) = "d"
+		
+		where (
+			bib# not in (
+				select bib# from bib where tag in ("191","791")
+			) 
+			and bib# not in (
+				select bib# from bib where tag = "099" and text like "%bDHU%"
+			)
+			or bib# in (
+				select bib# from bib where tag = "000" and substring(text,6,1) = "d"
+			)
 		)
 	}
 );
