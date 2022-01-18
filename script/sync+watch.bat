b37e59236c7bdeb3bc37d568ddce0f6d22f38d10
jbukhari <joe.bukhari@gmail.com>
2022-01-18 16:20:15 -0500

    Update sync+watch.bat

--- script/sync+watch.bat
+++ script/sync+watch.bat
@@ -1,6 +1,5 @@
 @echo off
 
-perl watch_kill.pl %MONGODB_CONNECT% y
-perl watch.pl -sM %MONGODB_CONNECT% -i 300
+perl watch_kill.pl %MONGODB_CONNECT% y && perl watch.pl -sM %MONGODB_CONNECT% -i 300
 
 cmd /k
